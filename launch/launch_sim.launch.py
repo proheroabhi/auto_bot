fa1cb515bac8d37f362a6e58217fd38df5cbb8ea
proheroabhi <abhilci8@gmail.com>
2026-03-27 20:26:47 +0530

    error solve in launch_sim

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -6,7 +6,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-form launch.substitution import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
